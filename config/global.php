69cdc3cfbb9f20e519c01a23a9e83bba9bdd9b7c
Milo <milagrosalfonzo@outlook.com.ar>
2020-06-02 13:27:24 -0300

    valores globales de conexion

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php 
+//ip de la pc servidor de base de datos
+define('DB_HOST', 'localhost');
+
+//nombre de la database
+define('DB_NAME', 'dbsistema');
+
+//usuario db
+define('DB_USERNAME', 'root');
+
+//contraseña db
+define('DB_PASSWORD', '');
+
+//define la codificacion de caracteres
+define('DB_ENCODE', 'utf8');
+
+//define una constante como nombre del proyecto
+define('PRO_NOMBRE', 'ITVentas');
+?>
