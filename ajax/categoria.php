fb415cddc8515de11e42efc2fb33e039e3217905
Milo <milagrosalfonzo@outlook.com.ar>
2020-06-03 13:49:07 -0300

    Funciones para desactivar o activar registros

--- ajax/categoria.php
+++ ajax/categoria.php
@@ -45,12 +45,19 @@ switch ($_GET["op"]){
 
         while ($reg=$rspta->fetch_object()){
             $data[]=array(
-                "0"=>'<button class="btn btn-warning" 
-                onclick="mostrar('.$reg->idcategoria.')"><i class="fa fa-pencil">
-                </i></button>', 
+                "0"=>($reg->condicion)?'<button class="btn btn-warning" 
+                onclick="mostrar('.$reg->idcategoria.')"> <i class="fa fa-pencil">
+                </i></button>'.
+                ' <button class="btn btn-danger" onclick="desactivar('.$reg->idcategoria.')">
+                <i class="fa fa-close"></i></button>': 
+                '<button class="btn btn-warning" onclick="mostrar('.$reg->idcategoria.')">
+                <i class="fa fa-pencil"></i></button>'.
+                ' <button class="btn btn-primary" onclick="activar('.$reg->idcategoria.')">
+                <i class="fa fa-check"></i></button>',
                 "1"=>$reg->nombre,
                 "2"=>$reg->descripcion,
-                "3"=>$reg->condicion
+                "3"=>($reg->condicion)?'<span class="label bg-green">Activado</span>':
+                '<span class="label bg-red">Desactivado</span>'
             );
             $results = array(
                 "sEcho"=>1, //informacion para el datatables
--- views/scripts/categoria.js
+++ views/scripts/categoria.js
@@ -96,5 +96,34 @@ function mostrar(idcategoria){
     })
 }
 
+//Funcion para desactivar registros
+function desactivar(idcategoria){
+
+    bootbox.confirm("¿Esta seguro de desactivar la categoria?", 
+    function(result){
+        if (result){
+            $.post("../ajax/categoria.php?op=desactivar", {idcategoria : idcategoria}, 
+            function(e){
+                bootbox.alert(e);
+                tabla.ajax.reload();
+            });
+        }
+    })
+}
+
+//Funcion para activar registros
+function activar(idcategoria){
+
+    bootbox.confirm("¿Esta seguro de activar la categoria?", 
+    function(result){
+        if (result){
+            $.post("../ajax/categoria.php?op=activar", {idcategoria : idcategoria}, 
+            function(e){
+                bootbox.alert(e);
+                tabla.ajax.reload();
+            });
+        }
+    })
+}
 
 init();
