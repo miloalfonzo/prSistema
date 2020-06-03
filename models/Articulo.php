591f2003420cc8ee6d30d2f3dc272787a2fd0e1f
Milo <milagrosalfonzo@outlook.com.ar>
2020-06-03 14:39:01 -0300

    modelo para los metodos de los articulos

---
+++ models/Articulo.php
@@ -0,0 +1,53 @@
+<?php
+
+require "../config/Conexion.php";
+
+Class Articulo {
+    //constructor
+    public function __construct(){
+
+    }
+
+    //method para insertar registros
+    public function insertar($idcategoria, $codigo, $nombre, $stock, $descripcion, $imagen){
+        $sql = "INSERT INTO articulo (idcategoria, codigo, nombre, stock, descripcion, imagen, condicion)
+        VALUES ('$idcategoria', '$codigo', '$nombre', '$stock', '$descripcion', '$imagen', '1')";
+        return ejecutarConsulta($sql);
+    }
+
+    //method para editar registros
+    public function editar($idarcitulo, $idcategoria, $codigo, $nombre, $stock, $descripcion, $imagen){
+        $sql = "UPDATE articulo SET idcategoria='$idcategoria', codigo='$codigo', nombre='$nombre', 
+        stock='$stock, descripcion='$descripcion', imagen='$imagen;
+        WHERE idarticulo='$idarticulo'";
+        return ejecutarConsulta($sql);
+    }
+
+    //method para desactivar registros
+    public function desactivar ($idarticulo){
+        $sql = "UPDATE articulo SET condicion='0' WHERE idarticulo='$idarticulo'";
+        return ejecutarConsulta($sql);
+    }
+
+    //method para activar registros
+    public function activar ($idarticulo){
+        $sql = "UPDATE articulo SET condicion='1' WHERE idarticulo='$idarticulo'";
+        return ejecutarConsulta($sql);
+    }
+
+    //implementar un method para mostrar los datos de un registro a modificar
+    public function mostrar($idarticulo){
+        $sql="SELECT * FROM articulo WHERE idarticulo='$idarticulo'";
+        return ejecutarConsultaSimpleFila($sql);
+    }
+
+    //method para listar los registros
+    public function listar(){
+        $sql="SELECT a.idarticulo, a.idcategoria, c.nombre as categoria,
+        a.codigo, a.stock, a.descripcion, a.imagen, a.condicion FROM 
+        articulo a INNER JOIN categoria c ON a.idcategoria=c.idcategoria";
+        return ejecutarConsulta($sql);
+    }
+}
+
+?>
