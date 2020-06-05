0a8fba4d39f7ddc877723a7f3c9d7497d94fdb71
Milo <milagrosalfonzo@outlook.com.ar>
2020-06-05 12:18:24 -0300

    metodos para manejar funciones dinamicas en la gestion de usuarios

---
+++ views/scripts/usuario.js
@@ -0,0 +1,154 @@
+var tabla;
+
+//función que se ejecuta al inicio
+function init(){
+    mostrarForm(false);
+    listar();
+
+    $("#formulario").on('submit', function(e){
+        guardaryeditar(e);
+    })
+
+    $("#imagenmuestra").hide();
+}
+
+// función limpiar
+function limpiar(){
+    $("#nombre").val("");
+    $("#num_documento").val("");
+    $("#direccion").val("");
+    $("#telefono").val("");
+    $("#email").val("");
+    $("#cargo").val("");
+    $("#logim").val("");
+    $("#clave").val("");
+    $("#imagenmuestra").attr("src", "");
+    $("#imagenactual").val("");
+    $("#idusuario").val("");
+}
+
+//funcion mostrar form
+function mostrarForm(flag){
+    limpiar();
+    if (flag){
+        $("#listadoregistros").hide();
+        $("#formularioregistros").show();
+        $("#btnGuardar").prop('disabled',false);
+        $("#btnagregar").hide();
+    } else {
+        $("#listadoregistros").show();
+        $("#formularioregistros").hide();
+        $("#btnagregar").show();
+    }
+}
+
+// Función cancelarForm
+function cancelarForm(){
+    limpiar();
+    mostrarForm(false);
+}
+
+// Función listar
+function listar(){
+    tabla=$("#tbllistado").dataTable({
+        "aProcessing": true, // activa el procesamiento del datatables
+        "aServerSide": true, // paginación y filtrado realizados por el servidor
+        dom: 'Bfrtip', // definimos los elementos del control de tabla
+        buttons: [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdf'
+                ],
+        "ajax": {
+                url: '../ajax/usuario.php?op=listar',
+                type: "get",
+                dataType: "json",
+                error: function(e){
+                    console.log(e.responseText);
+                }
+                },
+        "bDestroy": true,
+        "iDisplayLength": 5, //Paginación
+        "order": [[ 0, "desc" ]] //Ordenar (columna, orden)
+    }).DataTable();
+}
+
+//Función para guardar o editar
+function guardaryeditar(e){
+    e.preventDefault(); //no se activará la accion predeterminada del evento
+    $("#btnGuardar").prop("disabled", true);
+    var formData = new FormData($("#formulario")[0]);
+
+    $.ajax({
+        url: "../ajax/usuario.php?op=guardaryeditar",
+        type: 'POST',
+        data: formData,
+        contentType: false,
+        processData: false,
+
+        success: function(datos){
+            bootbox.alert(datos);
+            mostrarForm(false);
+            tabla.ajax.reload();
+        }
+    });
+    limpiar();
+}
+
+function mostrar(idusuario){
+    $.post("../ajax/usuario.php?op=mostrar", {idusuario : idusuario}, 
+    function(data, status){
+        
+        data = JSON.parse(data);
+        mostrarForm(true);
+
+        $("#nombre").val(data.nombre);
+        $("#tipo_documento").val(data.tipo_documento);
+        $("#tipo_documento").selectpicker('refresh');
+        $("#num_documento").val(data.num_documento);
+        $("#direccion").val(data.direccion);
+        $("#telefono").val(data.telefono);
+        $("#email").val(data.email);
+        $("#cargo").val(data.cargo);
+        $("#logim").val(data.login);
+        $("#clave").val(data.clave);
+        $("#imagenmuestra").show();
+        $("#imagenmuestra").attr("src", "../files/usuarios/"+data.imagen);
+        $("#imagenactual").val(data.imagen);
+        $("#idusuario").val(data.idusuario);
+    })
+}
+
+//Funcion para desactivar registros
+function desactivar(idusuario){
+
+    bootbox.confirm("¿Esta seguro de desactivar el usuario?", 
+    function(result){
+        if (result){
+            $.post("../ajax/usuario.php?op=desactivar", {idusuario : idusuario}, 
+            function(e){
+                bootbox.alert(e);
+                tabla.ajax.reload();
+            });
+        }
+    })
+}
+
+//Funcion para activar registros
+function activar(idusuario){
+
+    bootbox.confirm("¿Esta seguro de activar el usuario?", 
+    function(result){
+        if (result){
+            $.post("../ajax/usuario.php?op=activar", {idusuario : idusuario}, 
+            function(e){
+                bootbox.alert(e);
+                tabla.ajax.reload();
+            });
+        }
+    })
+}
+
+
+init();
